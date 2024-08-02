277ab392ff2f54e1ebc7b8ca6ecb155a12661a5f
Patryk Gajewski <patgaj15@gmail.com>
2024-08-02 18:34:22 +0200

    Dużo zmian

--- script.js
+++ script.js
@@ -2,174 +2,58 @@ import { rowData } from "./data.js";
 
 const body = document.body;
 // Nav
-const nav = document.createElement("nav");
-const spanNav = document.createElement("span");
-spanNav.textContent = `To see something interesting type "vader" or "yoda"`;
-const themeButton = document.createElement("button");
-themeButton.textContent = "Light side";
-themeButton.id = "themeButton";
+const nav = buildElement("nav");
+const spanNav = buildElement("span", undefined, undefined, "To hear something type 'vader' or 'yoda'");
+const themeButton = buildElement("button", "themeButton", undefined, "Light side");
 themeButton.addEventListener("click", changeSide);
 nav.append(spanNav, themeButton);
+
 // Buttons
-const divButtons = document.createElement("div");
-divButtons.id = "buttons";
-const tablesData = {};
+const divButtons = buildElement("div", "buttons");
 for (const key in rowData) {
-  tablesData.key = undefined;
-  const button = document.createElement("button");
-  button.id = key;
-  button.addEventListener("click", () => TestowaNazwa(key));
-  button.textContent = key.toUpperCase();
+  const button = buildElement("button", key, undefined, key.toUpperCase());
+  button.addEventListener("click", () => TestowaNazwaFunkcji(key));
   divButtons.appendChild(button);
 }
 
-// Wyciąganie danych
-// const category = "species";
-//
-// for (const index in rowData[category]) {
-//   //Index
-//   const row = rowData[category][index];
-//   for (const dataName in row) {
-//     //dataName - nazwa kolukmny
-//     //row[dataName] - wartość wiersza w kolumnie
-//   }
-// }
-
-// Tabela
-
-// TEST DODAWANIA REKORDÓW
-function TestowaNazwa(category) {
-  content.removeChild(content.lastElementChild);
-  const table = document.createElement("table");
-  const tHead = document.createElement("thead");
-  const tBody = document.createElement("tbody");
-  table.append(tHead, tBody);
-  const trHead = document.createElement("tr");
-  tHead.appendChild(trHead);
-  const thIndex = document.createElement("th");
-  thIndex.textContent = "ID";
-  trHead.appendChild(thIndex);
-  // Heading tabeli
-  let counter = 0;
-  for (const dataName in rowData[category][0]) {
-    if (skipColun(dataName, "episode_id", "opening_crawl")) {
-      continue;
-    }
-    const thColumn = document.createElement("th");
-    thColumn.textContent = dataName;
-    trHead.appendChild(thColumn);
-
-    counter += 1;
-    if (counter === 3) {
-      counter = 0;
-      break;
-    }
-  }
-  const thAction = document.createElement("th");
-  thAction.textContent = "Action";
-  trHead.appendChild(thAction);
-  // Content tabeli
-  for (const index in rowData[category]) {
-    //Index
-    const trBody = document.createElement("tr");
-    tBody.appendChild(trBody);
-    const tdIndex = document.createElement("td");
-    tdIndex.textContent = parseInt(index) + 1;
-    trBody.appendChild(tdIndex);
-    const row = rowData[category][index];
-    for (const dataName in row) {
-      if (skipColun(dataName, "episode_id", "opening_crawl")) {
-        continue;
-      }
-      // if (dataName === "episode_id" || dataName === "opening_crawl") {
-      //   continue;
-      // }
-      const td = document.createElement("td");
-      td.textContent = row[dataName];
-      trBody.appendChild(td);
-      counter += 1;
-      if (counter === 3) {
-        counter = 0;
-        break;
-      }
-    }
-  }
-  content.append(table);
-}
-
-// Skip Func
-function skipColun(currentColumn, ...unwantedColumns) {
-  const tab = [...unwantedColumns];
-  if (tab.includes(currentColumn)) {
-    return true;
-  }
-  return false;
-}
 // Content
-const content = document.createElement("div");
-content.id = "content";
+const content = buildElement("div", "content");
 // Logo
-const logo = document.createElement("img");
-logo.id = "logo";
-logo.src = ".\\img\\star_wars.png";
+const logo = buildElement("img", "logo", undefined, undefined, "./img/star_wars.png");
 content.appendChild(logo);
-// Vader
-const vader = document.createElement("img");
-vader.id = "vader";
-vader.src = ".\\img\\vader.png";
-// Yoda
-const yoda = document.createElement("img");
-yoda.id = "yoda";
-yoda.src = ".\\img\\yoda.png";
 
-body.append(nav, divButtons, content, vader, yoda);
+body.append(nav, divButtons, content);
 
-// Sound and Effect
-const vaderSound = document.createElement("audio");
-vaderSound.id = "vaderSound";
-vaderSound.src = ".\\mp3\\sound-for-vader.mp3";
-const yodaSound = document.createElement("audio");
-yodaSound.id = "yodaSound";
-yodaSound.src = ".\\mp3\\sound-for-yoda.mp3";
+// Sounds
+const vaderSound = buildElement("audio", "vaderSound", undefined, undefined, "./mp3/sound-for-vader.mp3");
+const yodaSound = buildElement("audio", "yodaSound", undefined, undefined, "./mp3/sound-for-yoda.mp3");
 let typed = "";
 const keywordVader = "vader";
 const keywordYoda = "yoda";
-const vaderSoundTime = 39000;
-const yodaSoundTime = 26000;
-const vaderDelay = 8000;
-const yodaDelay = 7000;
-yoda.style.animation = `appearance ${(yodaSoundTime - yodaDelay) / 1000}s linear`;
-vader.style.animation = `appearance ${(vaderSoundTime - vaderDelay) / 1000}s linear`;
 document.addEventListener("keydown", function (event) {
   typed += event.key.toLowerCase();
   if (typed.includes(keywordVader)) {
     typed = "";
     vaderSound.play();
-    yodaSound.pause();
-    yodaSound.currentTime = 0;
-    yoda.style.display = "none";
+    if (yodaSound.currentTime !== 0) {
+      yodaSound.pause();
+      yodaSound.currentTime = 0;
+    }
     setTimeout(() => {
-      vader.style.display = "block";
-    }, vaderDelay);
-    setTimeout(() => {
-      vader.style.display = "none";
       vaderSound.pause();
       vaderSound.currentTime = 0;
-    }, vaderSoundTime);
+    }, 30000);
   } else if (typed.includes(keywordYoda)) {
     typed = "";
     yodaSound.play();
-    vaderSound.pause();
-    vaderSound.currentTime = 0;
-    vader.style.display = "none";
+    if (vaderSound.currentTime !== 0) {
+      vaderSound.pause();
+      vaderSound.currentTime = 0;
+    }
     setTimeout(() => {
-      yoda.style.display = "block";
-    }, yodaDelay);
-    setTimeout(() => {
-      yoda.style.display = "none";
       yodaSound.pause();
       yodaSound.currentTime = 0;
-    }, yodaSoundTime);
+    }, 30000);
   }
   if (typed.length >= keywordVader.length) {
     typed = typed.substring(typed.length - (keywordVader.length - 1));
@@ -196,4 +80,101 @@ function changeSide() {
       button.style.color = "var(--black)";
     });
   }
+}
+
+function buildElement(typeElement, idElement, classElement, textElement, srcElement) {
+  const element = document.createElement(typeElement);
+  if (idElement !== undefined) {
+    element.id = idElement;
+  }
+  if (classElement !== undefined) {
+    element.class = classElement;
+  }
+  if (textElement !== undefined) {
+    element.textContent = textElement;
+  }
+  if (srcElement !== undefined) {
+    element.src = srcElement;
+  }
+  return element;
+}
+
+function TestowaNazwaFunkcji(category) {
+  // Border tylko dla aktualnej
+  const buttons = divButtons.children;
+  for (const button of buttons) {
+    button.style.border = "none";
+  }
+  const clickedButton = document.getElementById(category);
+  clickedButton.style.border = "solid var(--green) 2px";
+  content.removeChild(content.lastElementChild);
+  // Tworzenie tabeli
+  const table = buildElement("table");
+  const tHead = buildElement("thead");
+  const tBody = buildElement("tbody");
+  table.append(tHead, tBody);
+  const trHead = buildElement("tr");
+  tHead.appendChild(trHead);
+  const thIndex = buildElement("th", undefined, undefined, "ID");
+  trHead.appendChild(thIndex);
+  // dodawanie kolumn
+  let colums;
+  switch (category) {
+    case "vehicles":
+      colums = ["name", "model", "manufacturer", "created"];
+      break;
+    case "starships":
+      colums = ["name", "model", "manufacturer", "created"];
+      break;
+    case "planets":
+      colums = ["name", "climate", "terrain", "created"];
+      break;
+    case "people":
+      colums = ["name", "birth_year", "gender", "created"];
+      break;
+    case "species":
+      colums = ["name", "classification", "language", "created"];
+      break;
+    case "films":
+      colums = ["title", "director", "release_date", "created"];
+      break;
+  }
+
+  for (const dataName in rowData[category][0]) {
+    if (colums.includes(dataName)) {
+      const thColumn = buildElement("th", undefined, undefined, dataName);
+      trHead.appendChild(thColumn);
+    }
+  }
+  // dodanie kolumny Akcja
+  const thAction = buildElement("th", undefined, undefined, "action");
+  trHead.appendChild(thAction);
+  // Content tabeli
+  for (const index in rowData[category]) {
+    //Index
+    const trBody = buildElement("tr","trBody");
+    tBody.appendChild(trBody);
+    const tdIndex = buildElement("td", undefined, undefined, parseInt(index) + 1);
+    trBody.appendChild(tdIndex);
+    const row = rowData[category][index];
+    // dodawanie wartości rkordów
+    for (const dataName in row) {
+      if (colums.includes(dataName)) {
+        let record = row[dataName];
+        if (dataName === "created") {
+          record = record.slice(0,10);
+        }
+        const td = buildElement("td", undefined, undefined, record);
+        trBody.appendChild(td);
+      }
+    }
+    const tdAction = buildElement("td", "tdAction");
+    const dleteButton = buildElement("button", "deleteButton", undefined, "X");
+    const checkBoxRow = buildElement("input", "checkBoxRow");
+    checkBoxRow.type = "checkbox";
+    tdAction.append(dleteButton, checkBoxRow);
+    trBody.appendChild(tdAction);
+  }
+
+  content.append(table);
 }
